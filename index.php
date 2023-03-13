d2522769a1db42a6edc63bad1b6ee380cba25ceb
Eric <258753@cursist.rockopnh.nl>
2023-03-13 09:13:35 +0100

    Tailwind fixes, index changed to .php and start of Nav

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./dist/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    <?php include 'includes/navbar/navbar.php' ?>
     <p class="text-yellow-500 font-bold">Testing</pc>
 </body>
 </html>
