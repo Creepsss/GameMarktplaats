c5c191b5d9760b833905f4e779a1de61ce31cd6d
AstralPlays <milanbruul@gmail.com>
2023-03-13 09:29:41 +0100

    Merge did not work

---
+++ includes/metatags.php
@@ -0,0 +1,17 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="Game, Auction, Sell, Buy"/>
+<!-- <meta name="description" content="150 words"/> -->
+<meta name="language" content="EN">
+<meta name="author" content="Game-Marktplaats, email@gmail.com">
+<meta name="url" content="http://localhost.com">
+<meta name="identifier-URL" content="http://localhost.com">
+<meta name="category" content="">
+<meta name="coverage" content="Worldwide">
+<meta name="distribution" content="Global">
+<meta name="rating" content="General">
+<meta http-equiv="Expires" content="0">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+<title>Game-Bazaar</title>
