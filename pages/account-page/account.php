a9d16882da98993b3ea186a1986f811ad7fe6ed3
Gozers46 <henrygoos@icloud.com>
2023-03-13 21:56:55 +0100

    update

---
+++ pages/account-page/account.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require('../../includes/metatags.php'); ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../dist/output.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.checked {
+  color: orange;
+}
+</style>
+</head>
+<body>
+    <header>
+    <?php include ('../../includes/navbar/navbar.php'); ?>
+    </header>
+
+<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="avatar">
+<div class="dick text-white"><h1>User123</h1> </div>
+<div class="box1 text-white"><h2>Email:bla@gmail.com</h2></div>
+<div class="box2 text-white"><h2>Email:bla@gmail.com</h2></div>v
+</div>
+
+<div class="review">
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+</div>
+</body>
+</html>
