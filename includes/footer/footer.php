0ca471d622236356a4eca173133aa11c6b5fcc82
AstralPlays <milanbruul@gmail.com>
2023-03-20 11:59:49 +0100

    yes

---
+++ includes/footer/footer.php
@@ -0,0 +1,18 @@
+<footer class="bg-[#2D2D2D] px-10 pt-5">
+        <div class="widgets">
+            <div class="widget w-60">
+                <div class="widget-title border-b-2">
+                    <h3 class="text-white italic text-lg">Contact us:</h3>
+                </div>
+                <div class="content mt-2">
+                    <ul>
+                        <li><a class="text-white italic text-base" href="mailto:companyemail@gmail.com">Email: Companyemail@gmail.com</a></li>
+                        <li class="mt-2"><a class="text-white italic text-base" href="tell:+3164123123">Phone Number: 064123123</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="copy text-center py-5">
+            <span class="text-white italic text-lg">Copyright 2023</span>
+        </div>
+    </footer>
