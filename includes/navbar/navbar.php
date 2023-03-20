d37c307c487fb81812d66d89bd5d4c72f141acef
Eric <258753@cursist.rockopnh.nl>
2023-03-20 12:37:36 +0100

    Navigation account hover fix

--- includes/navbar/navbar.php
+++ includes/navbar/navbar.php
@@ -33,11 +33,13 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
             </svg>
         </p>
-        <div class="hidden group-hover/account:grid">
+
+        <div class="hidden group-hover/account:grid absolute right-0 bg-zinc-700 w-32 border-2 border-zinc-800 rounded-lg">
             <a class="#" href="#">Profile</a>
-            <a class="#" href="#">Listings</a>
-            <a class="#" href="#">Login</a>
-            <a class="#" href="#">Logout</a>
+            <a class="#" href="##">Listings</a>
+            <a class="#" href="###">Login</a>
+            <a class="#" href="####">Logout</a>
         </div>
+
     </div>
 </nav>
