aa3b0b93c5e57ad2423c651b8a17d3ee1724207c
Gozers46 <henrygoos@icloud.com>
2023-03-30 12:33:36 +0200

    update

--- pages/account-page/accountgames.php
+++ pages/account-page/accountgames.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../../dist/output.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 .checked {
   color: orange;
@@ -23,14 +25,20 @@
     <div class="zijkantonder"></div>
 </div>
 <div class="dick text-white">
-</div>
+Reviews:1
+
+<div class="rect"><i class='far fa-comment-alt'>Chat</i>
+<i class='fas fa-phone-alt'>064345141</i>
 <div class="user">   
 <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="avatar">
+<h2 class="username text-white">User293401 </h2>
+</div>
+
+</div>
 
 </div>
 <div class="rectangle">
 <div class="review">
-</div>
 </div>
 
     <span class="fa fa-star checked"></span>
