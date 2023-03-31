eb8bde4bc4b94b1435c056ccd7914d79764debfb
Gozers46 <henrygoos@icloud.com>
2023-03-31 11:55:45 +0200

    dropdownmenu en meer

--- pages/account-page/accountgames.php
+++ pages/account-page/accountgames.php
@@ -24,32 +24,47 @@
     <div class="zijkantboven"></div>
     <div class="zijkantonder"></div>
 </div>
+<h2 class="gamesale text-white">Games on sale by user:4</h2>
 <div class="dick text-white">
-Reviews:1
-
-<div class="rect"><i class='far fa-comment-alt'>Chat</i>
-<i class='fas fa-phone-alt'>064345141</i>
-<div class="user">   
-<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="avatar">
-<h2 class="username text-white">User293401 </h2>
-</div>
-
-</div>
-
-</div>
+<h2 class="rev">Reviews:1</h2>
 <div class="rectangle">
 <div class="review">
 </div>
 
     <span class="fa fa-star checked"></span>
 <span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
 <span class="fa fa-star"></span>
 <span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+</div>
+
+<div class="user">   
+<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="avatar">
+<h2 class="n" text-white">User293401 </h2>
+</div>
+</div>
+<div class="max">
+<div class="max-w-lg mx-auto p-8">
+  <details class="open:bg-white dark:open:bg-slate-900 open:ring-1 open:ring-black/5 dark:open:ring-white/10 open:shadow-lg p-6 rounded-lg" open>
+    <summary class="text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none">
+    Click to  connect to user
+    </summary>
+    <div class="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+      <p> <i class='far fa-comment-alt'>Chat</i> <i class='fas fa-phone-alt'>064345141</i>
+      <i class="fa fa-envelope">user@gmail.com</i>                     </p>
+    </div>
+    </div>
+  </details>
+</div>
+</div>
+
 </div>
 <div class="games  text-white">
     <div>Valorent Key | Global steam key<div class="euro"<p>4,5 euro</p></div></div>
     <div>Valorent Key | Global steam key<div class="euro"<p>4,5 euro</p> </div></div>
+    <div>Valorent Key | Global steam key<div class="euro"<p>4,5 euro</p></div></div>
+    <div>Valorent Key | Global steam key<div class="euro"<p>4,5 euro</p> </div></div>
+
 </div> 
 </body>
 </html>
